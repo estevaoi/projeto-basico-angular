c88cc15b4903f1c346602bddcdf9ba9b47aefd69
estevaoi <estevaoi>
2023-04-17 15:08:55 -0300

    default components global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { ModalDeleteComponent } from './components/modal-delete/modal-delete.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { ModalBackComponent } from './components/modal-back/modal-back.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BreadcrumbComponent,
+    ModalDeleteComponent,
+    FooterComponent,
+    MessagesComponent,
+    ModalBackComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/modal-back/modal-back.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-back',
+  templateUrl: './modal-back.component.html',
+  styleUrls: ['./modal-back.component.css']
+})
+export class ModalBackComponent {
+
+}
